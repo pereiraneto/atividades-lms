bcd3f917864a91a32c81f8760b81f5b372705fc9
pereiraneto <pereira.neto.12@gmail.com>
2017-10-17 17:12:54 -0200

    Atualizei transições

--- Atividade 6/script.js
+++ Atividade 6/script.js
@@ -4,9 +4,9 @@ let width = screen.width;
 
 function abrirMenu(){
   document.getElementById('nav-vertical').style.transform="translate(0%)";
-  document.getElementById('nav-vertical').style.transition="all 1s";
+  document.getElementById('nav-vertical').style.transition="all 500ms";
   document.getElementById('conteudo').style.transform="translate(20%)";
-  document.getElementById('conteudo').style.transition="all 1s";
+  document.getElementById('conteudo').style.transition="all 500ms";
   if(width >= 480){
     document.getElementById('conteudo').style.marginLeft="0%";
   }
@@ -14,9 +14,9 @@ function abrirMenu(){
 
 function fecharMenu() {
   document.getElementById('nav-vertical').style.transform="translate(-100%)";
-  document.getElementById('nav-vertical').style.transition="all 1s";
+  document.getElementById('nav-vertical').style.transition="all 500ms";
   document.getElementById('conteudo').style.transform="translate(0%)";
-  document.getElementById('conteudo').style.transition="all 1s";
+  document.getElementById('conteudo').style.transition="all 500ms";
 }
 
 sanduiche.addEventListener("click", abrirMenu);
@@ -32,7 +32,7 @@ function abrirAcordeon(i){
   contentAcordeons[i].classList.add("active");
   let headers = document.querySelectorAll("#id-content-acordeon");
   for (let i = 0; i < headerAcordeons.length; i++) {
-    headers[i].style.transition="all 600ms";
+    headers[i].style.transition="all 500ms";
   }
   active = i;
 }
